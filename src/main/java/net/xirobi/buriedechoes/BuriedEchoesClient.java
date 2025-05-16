b9b8e4afde642babbf59dbf598f67294ff8f5779
XiRobi <ryandpatterson59@gmail.com>
2025-05-16 03:52:34 +0100

    test

---
+++ src/main/java/net/xirobi/buriedechoes/BuriedEchoesClient.java
@@ -0,0 +1,10 @@
+package net.xirobi.buriedechoes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BuriedEchoesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/xirobi/buriedechoes/BuriedEchoesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.xirobi.buriedechoes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BuriedEchoesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
